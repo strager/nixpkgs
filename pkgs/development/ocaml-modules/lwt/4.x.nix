9a7bf70ce4526cc4ad5acc01e49000730f33cb66
Matthew Glazar <strager.nds@gmail.com>
2019-11-19 22:18:40 -0800

    IT COMPILES

--- pkgs/development/ocaml-modules/lwt/4.x.nix
+++ pkgs/development/ocaml-modules/lwt/4.x.nix
@@ -1,5 +1,6 @@
-{ lib, fetchzip, pkgconfig, ncurses, libev, buildDunePackage, ocaml
+{ lib, fetchFromGitHub, pkgconfig, ncurses, libev, buildDunePackage, ocaml
 , cppo, ocaml-migrate-parsetree, ppx_tools_versioned, result
+, bisect_ppx
 , mmap, seq
 }:
 
@@ -9,15 +10,24 @@ buildDunePackage rec {
   pname = "lwt";
   version = "4.2.1";
 
-  src = fetchzip {
-    url = "https://github.com/ocsigen/${pname}/archive/${version}.tar.gz";
+  src = fetchFromGitHub {
+    owner = "ocsigen";
+    repo = pname;
+    rev = version;
     sha256 = "1hz24fyhpm7d6603v399pgxvdl236srwagqja41ljvjx83y10ysr";
   };
 
-  postPatch = ''
-    substituteInPlace lwt.opam \
-    --replace 'version: "dev"' 'version: "${version}"'
-  '';
+#  src = fetchFromGitHub {
+#    owner = "rvantonder";
+#    repo = pname;
+#    rev = "1d58b7993961d4a1f668ffbae94450145dd30677";
+#    sha256 = "0mmkvglc805rmvgwciwng3fqsa69gl0zmp590k1nrs1vjmhzzfsd";
+#  };
+
+#  postPatch = ''
+#    substituteInPlace lwt.opam \
+#    --replace 'version: "dev"' 'version: "${version}"'
+#  '';
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ cppo ocaml-migrate-parsetree ppx_tools_versioned ]
--- pkgs/development/tools/comby/default.nix
+++ pkgs/development/tools/comby/default.nix
@@ -2,7 +2,8 @@
 
 ocamlPackages.buildDunePackage rec {
   pname = "comby";
-  version = "0.11.1";
+  version = "0.11.0";
+  #version = "0.9.1";
 
   minimumOCamlVersion = "4.08.1";
 
@@ -10,21 +11,26 @@ ocamlPackages.buildDunePackage rec {
     owner = "comby-tools";
     repo = pname;
     rev = version;
-    sha256 = "01kc07zp2xbzbzcmx7znvywg6bcr69sl5nia5bmadlmmbp9yhyiw";
+    #sha256 = "01kc07zp2xbzbzcmx7znvywg6bcr69sl5nia5bmadlmmbp9yhyiw"; # 0.11.1
+    sha256 = "02r4h94myv54savz0r8rsx2y76p2237ja7s4g1nryp7vg4mm7q2b"; # 0.11.0
+    #sha256 = "1sd7bjy4432bh7xxzcd6g304r9smgg1l8pwv6c93fn4asjfsg1p0"; # 0.8.0
+    #sha256 = "1pb3i05smsxx5sy6m41hginrif2cnv1nsals7m0x7amyf1sm774r";
   };
 
-  patches = [ ./ppx-conditional.patch ];
+  patches = [ ./ppx-conditional.patch ./equality.patch ./lwt-unix.patch ./missing-dependency.patch ];
+  #patches = [ ./ppx-conditional-0.9.1.patch ];
 
   buildInputs = with ocamlPackages; [
-    #lambda-term
-    #lwt
     angstrom
     bisect_ppx
     camlzip
     core
     hack_parallel
+    janeStreet.shell
+    lambdaTerm # @@@ is this really necessary?
     lwt_react
     mparser-comby
+    ocaml_lwt
     ocaml_oasis
     ocaml_pcre
     opium
